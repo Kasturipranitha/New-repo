836f30a3cd2c447dc6c7b5fe99211805370731d5
Kasturipranitha <kasturipranitha@gmail.com>
2026-01-23 12:26:17 +0530

    Update app.java

--- app.java
+++ app.java
@@ -6,3 +6,14 @@ public static void main(String[] args) {
 System.out.println("Hello, World!");
 }
 }
+
+#dev 2 pushed code
+
+  // This is a simple Java program
+public class HelloWorld {
+   // The main method is the entry point of the program
+   public static void main(String[] args) {
+       // Print "Hello, World!" to the console
+       System.out.println("Hello, World!");
+   }
+}
